360c83aadc315293943a49566dd7d59cb12d420b
Sagar Goyal <sagargoyal2015@gmail.com>
2021-11-14 03:21:51 +0530

    Added Loader component

--- screens/Coin.js
+++ screens/Coin.js
@@ -9,30 +9,30 @@ import {
 import Chart from "../component/Chart";
 import { getMarketData } from "../services/cryptoService";
 import Constants from "expo-constants";
+import Loader from "../component/Loader";
 
 export default function Coin() {
   const [selectedCoinData, setSelectedCoinData] = useState(null);
 
   var SI_SYMBOL = ["", "k", "M", "B", "T", "P", "E"];
 
-    function setNum(number){
+  function setNum(number) {
+    // what tier? (determines SI symbol)
+    var tier = (Math.log10(Math.abs(number)) / 3) | 0;
 
-        // what tier? (determines SI symbol)
-        var tier = Math.log10(Math.abs(number)) / 3 | 0;
+    // if zero, we don't need a suffix
+    if (tier == 0) return number;
 
-        // if zero, we don't need a suffix
-        if(tier == 0) return number;
+    // get suffix and determine scale
+    var suffix = SI_SYMBOL[tier];
+    var scale = Math.pow(10, tier * 3);
 
-        // get suffix and determine scale
-        var suffix = SI_SYMBOL[tier];
-        var scale = Math.pow(10, tier * 3);
+    // scale the number
+    var scaled = number / scale;
 
-        // scale the number
-        var scaled = number / scale;
-
-        // format number and add suffix
-        return scaled.toFixed(1) +" "+ suffix;
-    }
+    // format number and add suffix
+    return scaled.toFixed(1) + " " + suffix;
+  }
 
   useEffect(() => {
     const fetchMarketData = async () => {
@@ -44,45 +44,188 @@ export default function Coin() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ width: "100%" }}>
-        <Text style={{ textAlign: "center", fontSize: 30, fontWeight: "bold" }}>
-          {selectedCoinData?.name}/{selectedCoinData?.symbol}
-        </Text>
-      </View>
-      <View>
-        {selectedCoinData ? (
-          <Chart
-            currentPrice={selectedCoinData.current_price}
-            logoUrl={selectedCoinData.image}
-            name={selectedCoinData.name}
-            symbol={selectedCoinData.symbol}
-            priceChangePercentage7d={
-              selectedCoinData.price_change_percentage_7d_in_currency
-            }
-            sparkline={selectedCoinData?.sparkline_in_7d.price}
-            high={selectedCoinData?.high_24h}
-            low={selectedCoinData?.low_24h}
-          />
-        ) : null}
-      </View>
-      <View style={{flex:4}}>
-      <View style={{flexDirection:"row", marginLeft:10, marginRight:10, marginBottom:4, marginTop:5, justifyContent:"space-between", height:"75%"}}>
-        <View style={{width:"50%", borderRightWidth:1, borderColor:"#dbdbdb", justifyContent:"space-evenly", paddingRight:10}}>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>Rank</Text><Text style={{fontWeight: "bold"}}>#{selectedCoinData?.market_cap_rank}</Text></View>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>Volume</Text><Text style={{fontWeight: "bold"}}>$ {setNum(selectedCoinData?.total_volume)}</Text></View>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>ATH</Text><Text style={{fontWeight: "bold"}}>$ {selectedCoinData?.ath}</Text></View>
-        </View>
-        <View style={{width:"50%", borderColor:"grey", justifyContent:"space-evenly", paddingLeft:5}}>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>Market Cap</Text><Text style={{fontWeight: "bold"}}>{setNum(selectedCoinData?.market_cap)}</Text></View>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>Total Supply</Text><Text style={{fontWeight: "bold"}}>{setNum(selectedCoinData?.total_supply)}</Text></View>
-          <View style={{flexDirection: "row", justifyContent:"space-between", margin:3}}><Text style={{color:"grey"}}>Maximum Supply</Text><Text style={{fontWeight: "bold"}}>{setNum(selectedCoinData?.max_supply)}</Text></View>
-        </View>
-      </View>
-      <View style={{height:"25%", flexDirection: "row", justifyContent:"space-between"}}>
-          <TouchableOpacity style={{width:"50%", backgroundColor:"blue",alignItems:"center", justifyContent:"center", borderRightWidth:1, borderColor:"white"}}><Text style={{fontWeight:"bold",textAlign:"center", fontSize:20, color:"white"}}>BUY</Text></TouchableOpacity>
-          <TouchableOpacity style={{width:"50%", backgroundColor:"blue",alignItems:"center", justifyContent:"center"}}><Text style={{fontWeight:"bold",textAlign:"center", fontSize:20, color:"white"}}>SELL</Text></TouchableOpacity>
-      </View>
-      </View>
+      {selectedCoinData ? (
+        <>
+          <View style={{ width: "100%" }}>
+            <Text
+              style={{ textAlign: "center", fontSize: 30, fontWeight: "bold" }}
+            >
+              {selectedCoinData?.name}/{selectedCoinData?.symbol}
+            </Text>
+          </View>
+          <View>
+            {selectedCoinData ? (
+              <Chart
+                currentPrice={selectedCoinData.current_price}
+                logoUrl={selectedCoinData.image}
+                name={selectedCoinData.name}
+                symbol={selectedCoinData.symbol}
+                priceChangePercentage7d={
+                  selectedCoinData.price_change_percentage_7d_in_currency
+                }
+                sparkline={selectedCoinData?.sparkline_in_7d.price}
+                high={selectedCoinData?.high_24h}
+                low={selectedCoinData?.low_24h}
+              />
+            ) : null}
+          </View>
+          <View style={{ flex: 4 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                marginLeft: 10,
+                marginRight: 10,
+                marginBottom: 4,
+                marginTop: 5,
+                justifyContent: "space-between",
+                height: "75%",
+              }}
+            >
+              <View
+                style={{
+                  width: "50%",
+                  borderRightWidth: 1,
+                  borderColor: "#dbdbdb",
+                  justifyContent: "space-evenly",
+                  paddingRight: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>Rank</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    #{selectedCoinData?.market_cap_rank}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>Volume</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    $ {setNum(selectedCoinData?.total_volume)}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>ATH</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    $ {selectedCoinData?.ath}
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  width: "50%",
+                  borderColor: "grey",
+                  justifyContent: "space-evenly",
+                  paddingLeft: 5,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>Market Cap</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    {setNum(selectedCoinData?.market_cap)}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>Total Supply</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    {setNum(selectedCoinData?.total_supply)}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    margin: 3,
+                  }}
+                >
+                  <Text style={{ color: "grey" }}>Maximum Supply</Text>
+                  <Text style={{ fontWeight: "bold" }}>
+                    {setNum(selectedCoinData?.max_supply)}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                height: "25%",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  width: "50%",
+                  backgroundColor: "blue",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRightWidth: 1,
+                  borderColor: "white",
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    fontSize: 20,
+                    color: "white",
+                  }}
+                >
+                  BUY
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  width: "50%",
+                  backgroundColor: "blue",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    fontSize: 20,
+                    color: "white",
+                  }}
+                >
+                  SELL
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      ) : (
+        <Loader />
+      )}
     </SafeAreaView>
   );
 }
